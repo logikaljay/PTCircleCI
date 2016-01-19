380a4a0c148bb773537ae1e0e7edf6f861951797
Jay Baker <logikal@gmail.com>
2016-01-19 16:18:42 +1300

    chore(Build): ughghdshgh

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -3,11 +3,12 @@
 KEYCHAIN_PASSWORD=circleci
 
 security create-keychain -p $KEYCHAIN_PASSWORD ios-build.keychain
-security import ./scripts/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 security list-keychain -s ~/Library/Keychains/ios-build.keychain
 security unlock-keychain -p $KEYCHAIN_PASSWORD ~/Library/Keychains/ios-build.keychain
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./scripts/profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
+
